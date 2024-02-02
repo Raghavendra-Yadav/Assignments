d20cff943d596b9d0860b649e0330800a1cc136a
Raghavendra Yadav <graghavendra.yadav@gmail.com>
2024-02-02 02:32:55 -0500

    insertOne Method

--- Classwork01.js
+++ Classwork01.js
@@ -29,3 +29,40 @@ db.Details.insertOne({
   acknowledged: true,
   insertedId: 351
 } */
+
+// Second Query
+// Creating a new collection named 'Post'
+db.createCollection('Post')
+
+// Inserting a document into the 'Post' collection 
+// Using the 'insertOne' method
+db.Post.insertOne({
+    "id": "POST_ID", 
+    "title": "TITLE_OF_POST", 
+    "description": "POST_DESCRIPTION", 
+    "by": "POST_BY", 
+    "url": "URL_OF_POST", 
+    "tags": ["TAG1", "TAG2", "TAG3"], // Array of tags associated with the post
+    "likes": "TOTAL_LIKES", 
+    "comments": [
+        {
+            "user": "COMMENT_BY", 
+            "message": "TEXT", 
+            "dateCreated": "DATE_TIME", 
+            "like": "LIKES" 
+        },
+        {
+            "user": "COMMENT_BY",
+            "message": "TEXT",
+            "dateCreated": "DATE_TIME",
+            "like": "LIKES"
+        }
+    ]
+})
+
+//Output of the insertOne method
+
+/* Output: {
+  acknowledged: true,
+  insertedId: ObjectId('65bc69751c3679bd62609612')
+} */
