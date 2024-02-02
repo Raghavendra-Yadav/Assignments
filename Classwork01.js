b07d0202e3a4b40aba6ee5ae6112e9a1ae6cb576
Raghavendra Yadav <graghavendra.yadav@gmail.com>
2024-02-02 02:41:40 -0500

    insertMany Method

--- Classwork01.js
+++ Classwork01.js
@@ -66,3 +66,65 @@ db.Post.insertOne({
   acknowledged: true,
   insertedId: ObjectId('65bc69751c3679bd62609612')
 } */
+
+//Query 3
+// Inserting a document into the 'Posts' collection 
+// Using the 'insertMany' method
+db.Posts.insertMany([
+    {
+        "id": "POST_ID_1",
+        "title": "TITLE_OF_POST_1",
+        "description": "POST_DESCRIPTION_1",
+        "by": "POST_BY_1",
+        "url": "URL_OF_POST_1",
+        "tags": ["TAG1", "TAG2", "TAG3"],
+        "likes": "TOTAL_LIKES_1",
+        "comments": [
+            {
+                "user": "COMMENT_BY_1",
+                "message": "TEXT_1",
+                "dateCreated": "DATE_TIME_1",
+                "like": "LIKES_1"
+            },
+            {
+                "user": "COMMENT_BY_1",
+                "message": "TEXT_1",
+                "dateCreated": "DATE_TIME_1",
+                "like": "LIKES_1"
+            }
+        ]
+    },
+    {
+        "id": "POST_ID_2",
+        "title": "TITLE_OF_POST_2",
+        "description": "POST_DESCRIPTION_2",
+        "by": "POST_BY_2",
+        "url": "URL_OF_POST_2",
+        "tags": ["TAG4", "TAG5", "TAG6"],
+        "likes": "TOTAL_LIKES_2",
+        "comments": [
+            {
+                "user": "COMMENT_BY_2",
+                "message": "TEXT_2",
+                "dateCreated": "DATE_TIME_2",
+                "like": "LIKES_2"
+            },
+            {
+                "user": "COMMENT_BY_2",
+                "message": "TEXT_2",
+                "dateCreated": "DATE_TIME_2",
+                "like": "LIKES_2"
+            }
+        ]
+    }
+])
+
+//Output of the insertMany method
+
+/* Output: {
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('65bc9c18f440bda6f2d5fbea'),
+    '1': ObjectId('65bc9c18f440bda6f2d5fbeb')
+  }
+} */
