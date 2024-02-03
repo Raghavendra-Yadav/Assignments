1be7019a8254a6a8820f406631a2861f3f39b1ba
Raghavendra Yadav <graghavendra.yadav@gmail.com>
2024-02-03 00:43:42 -0500

    Mongopy Python Script

--- Classwork02.js
+++ Classwork02.js
@@ -159,3 +159,28 @@ db.UniversityData.find({
       ]
     }
   });
+
+//Mongopy
+
+# Import necessary modules from pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# MongoDB connection URI
+uri = "mongodb+srv://graghavendrayadav:Raghu@cluster0.gwjlwmk.mongodb.net/"
+
+# Connect to MongoDB
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    '''for db_name in client.list_database_names():
+        print(db_name)*/''' #once sucessfully established connection
+except Exception as e:
+    print(e)
+
+/*Output:
+Pinged your deployment. You successfully connected to MongoDB!
+*/
