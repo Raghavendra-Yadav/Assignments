7d343c8bdd316b1605dd963884026b9cb4d50fe3
Raghavendra Yadav <graghavendra.yadav@gmail.com>
2024-02-03 10:31:25 -0500

    Classwork 1 - Completed

--- Classwork02.js
+++
@@ -1,9 +0,0 @@
-/*
-  MongoDB Query Operations
-
-  Description:
-  This script contains MongoDB queries using the MongoDB shell.
-  It demonstrates various queries, including AND, OR, NOR, NOT, AND-OR-OR, Element Match, document finding, insertion with insertOne and insertMany, and more.
-*/
-
-
